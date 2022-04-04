a4b282862b72ba599fbf1a659d4ee074af99c50a
yuchanns <me@yuchanns.xyz>
2022-04-04 18:29:18 +0800

    remove useless methods

--- src/facades/TracerElasticApm.php
+++ src/facades/TracerElasticApm.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class TracerElasticApm
  * @package Yuchanns\ElasticApmTracer\Facades
- * @method static getSpan($name, $type, $subType = null, $action = null)
- * @method static endSpan($name)
+ * @method static inject(array $header)
  */
 class TracerElasticApm extends Facade
 {
